36844f2f30cecd0f2af4487b789c2c4499f1a951
Thoss24 <t.fogarty24@outlook.com>
2024-01-01 14:20:25 +0000

    routing setup

---
+++ index.js
@@ -0,0 +1,33 @@
+const http = require("node:http");
+const url = require("url")
+const fs = require("fs");
+
+const page404 = fs.readFileSync("./project/404.html", "utf-8", (err, data) => {
+    if (err) {
+        throw err
+    } return data
+})
+
+http.createServer((req, res) => {
+   const q = url.parse(req.url, true);
+   
+   let filename = ""
+   if (q.pathname === "/") {
+    filename = "." + "/project/index.html";
+   } else {
+    filename = "." + "/project" + q.pathname
+    console.log(q.pathname)
+   }
+
+   fs.readFile(filename, (err, data) => {
+    if (err) {
+        res.writeHead(404, { "Content-Type": "text/html"});
+        res.write(page404);
+        return res.end();
+    } else {
+        res.writeHead(200, { "Content-Type": "text/html"});
+        res.write(data);
+        return res.end();
+    }
+   })
+}).listen(8080)
