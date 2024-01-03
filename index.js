79429682ebdc895ab4c837cafacdd0970dcc8710
Thoss24 <t.fogarty24@outlook.com>
2024-01-03 21:26:24 +0000

    creating server from scratch

--- index.js
+++ index.js
@@ -2,26 +2,34 @@ const http = require("node:http");
 const url = require("url")
 const fs = require("fs");
 
-http.createServer((req, res) => {
-   const q = url.parse(req.url, true);
-   
-   let filename = ""
-   if (q.pathname === "/") {
-    filename = "." + "/project/index.html";
-   } else {
-    filename = "." + "/project" + q.pathname + ".html"
-    console.log(q.pathname)
-   }
-
-   fs.readFile(filename, (err, data) => {
+const page404 = fs.readFileSync('project/404.html', 'utf8', (err, data) => {
+    console.log(data)
     if (err) {
-        res.writeHead(404, { "Content-Type": "text/html"});
-        res.write(page404);
-        return res.end();
+        //console.log(err)
     } else {
-        res.writeHead(200, { "Content-Type": "text/html"});
-        res.write(data);
-        return res.end();
+        return data
     }
-   })
+});
+
+http.createServer((req, res) => {
+    const q = url.parse(req.url, true);
+    let filename = ""
+
+    if (q.pathname === "/") {
+        filename = "project/index.html"
+    } else {
+        filename = "project/" + q.pathname + ".html";
+    };
+
+    fs.readFile(filename, 'utf8', (err, data) => {
+        if (err) {
+            res.writeHead(404, {"Content-Type": 'text/html'})
+            res.write(page404)
+            return res.end()
+        } else {
+            res.writeHead(200, {"Content-Type": 'text/html'})
+            res.write(data)
+            return res.end()
+        }
+    })
 }).listen(8080)
